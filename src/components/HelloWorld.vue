a6e8cc7c517265ed46b7f319a8f06a36699532a4
yossiee <38056766+yossiee@users.noreply.github.com>
2020-05-12 21:00:26 +0900

    Create a project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    {{ msg }}
+    <form>
+      <button>Add task</button>
+      <button>Delete or Finished task</button>
+      <p>input: <input type="text"></p>
+      <p>task:</p>
+    </form>
+    <div class="task-list">
+      <label class="task-list__item" v-for="todo in todos" :key="todo.text">
+        <input type="checkbox"><button>Edit</button>{{ todo.text }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      todos: [
+        {
+          text: 'vue-router', done: false
+        },
+        {
+          text: 'vuex', done: false
+        },
+        {
+          text: 'vue-loader', done: false
+        },
+        {
+          text: 'awesome-vue', done: true
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@mixin flex-vender() {
+  display: flex;
+  display: -webkit-flex;
+  display: -moz-flex;
+  display: -ms-flex;
+  display: -o-flex;
+}
+.task-list {
+  @include flex-vender;
+  flex-direction: column;
+  align-items: center;
+  &__item {
+    width: 270px;
+    text-align: left;
+    $element: #{&};
+    &--checked {
+      @extend #{$element};
+      color: #85a6c6;
+    }
+  }
+}
+</style>
